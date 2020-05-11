115883be2b549dad1df7c3780a74e7513b019a62
WhiteFerrari666 <der_moaddin@yahoo.de>
2020-05-11 17:36:22 +0200

    Initial commit

---
+++ src/test/java/com/Medication/Medication/MedicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Medication.Medication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
