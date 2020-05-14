a18f6488e71e3c8c48d9932114a6e0157cb31514
WhiteFerrari666 <der_moaddin@yahoo.de>
2020-05-14 11:40:26 +0200

    Spring Security Default Credentials vergeben
    user: user
    password: password
    
    Außerdem: Eclipse Code Formatter eingefügt und Klassen formatiert.

--- src/main/java/com/Medication/Medication/MedicationApplication.java
+++ src/main/java/com/Medication/Medication/MedicationApplication.java
@@ -1,33 +1,37 @@
 package com.Medication.Medication;
 
-import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "controller")
 public class MedicationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MedicationApplication.class, args);
-    }
+	public static void main(String[] args) {
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
+		SpringApplication.run(MedicationApplication.class, args);
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+	}
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
 
-        };
-    }
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 }
--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,13 +1,19 @@
 package controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Ich vermute, hier entsteht unsere "Seite". Der Inhalt wird angezeigt, wenn
+ * man die Credentials richtig eingegeben hat :)
+ */
 @RestController
+@EnableAutoConfiguration
 public class TestController {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Spring boot is working!";
-    }
+	@RequestMapping("/")
+	public String home() {
+		return "Spring boot is working!";
+	}
 }
