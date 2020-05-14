7136702584b6057d1c246acaa99f77382c5150f2
WhiteFerrari666 <der_moaddin@yahoo.de>
2020-05-14 15:53:13 +0200

    aufgeräumt

---
+++ src/main/java/controller/WebController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+}
