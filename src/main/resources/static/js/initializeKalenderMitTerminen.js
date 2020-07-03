fca3e57c3278a2a011ac214b0905f498b9ffdab3
WhiteFerrari666 <der_moaddin@yahoo.de>
2020-07-03 18:24:29 +0200

    kleine Umformatierung, um das Problem zu verdeutlichen

--- src/main/resources/static/js/initializeKalenderMitTerminen.js
+++ src/main/resources/static/js/initializeKalenderMitTerminen.js
@@ -4,14 +4,17 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 var termine = JSON.parse(request.response)
+
+var settings = {};
+var element = document.getElementById('kalender');
+
 var events = termine;
 
-
-/*[
+/*
+var events = [
     // in diesem Format sollen die geparsten Daten vorliegen, damit der Kalender sie unten verarbeiten/aufnehmen kann
     // wie kriege ich die aus dem JSON in diese Variable bzw. dieses Format?
 
-   //termine
 
     {"Date": new Date(2020, 7, 10), "Title": "Arzttermin um 15:30", "Link": "https://www.docmuenster.de/"},
     {"Date": new Date(2020, 5, 26), 'Title': 'Aspirin nehmen', 'Link': 'https://www.aspirin.de/'},
@@ -20,7 +23,6 @@ var events = termine;
         'Title': 'Pollensaison geht los',
         'Link': 'https://allergie.hexal.de/pollenflug/pollenflugkalender/'
     }
-];*/
-var settings = {};
-var element = document.getElementById('kalender');
+];
+*/
 caleandar(element, events, settings);
