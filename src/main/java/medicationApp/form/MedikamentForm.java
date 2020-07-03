d5439dd3d927575a258faf4ee9f5d766b949dc8e
kathik00 <kathrin.hartmann00@outlook.com>
2020-07-03 12:50:56 +0200

    medikamentForm

--- src/main/java/medicationApp/form/MedikamentForm.java
+++ src/main/java/medicationApp/form/MedikamentForm.java
@@ -1,22 +1,32 @@
 package medicationApp.form;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class MedikamentForm {
-	private String name;
-	private int dosis;
 
-	public int getDosis() {
-		return dosis;
-	}
 
-	public void setDosis(int dosis) {
-		this.dosis = dosis;
-	}
+    private String name;
+    private long dosis;
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public long getDosis() {
+
+        return dosis;
+    }
+
+    public void setDosis(long dosis) {
+        this.dosis = dosis;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
 }
